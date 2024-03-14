0d2535600f273f8132c27312d9c2636bad15bccd
Dmytro20777 <08dmutro11@gmail.com>
2024-03-14 09:45:58 +0200

    Homewrok02

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -13,7 +13,10 @@ type AllType = {
 
 
 
-function compare <T extends AllType>(top: T, bottom: T):AllType  {
+function compare<
+  T extends Pick<AllType, "name" | "color">,
+  U extends Pick<AllType, "position" | "weight">
+>(top: T, bottom: U): AllType  {
   return {
     name: top.name,
     color: top.color,
